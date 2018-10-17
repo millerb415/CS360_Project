e92a216f13541400b5c75b0840698412b251faea
millerb415 <33108827+millerb415@users.noreply.github.com>
2018-10-16 18:29:47 -0700

    made changes

--- src/apuParse.java
+++ src/apuParse.java
@@ -1,63 +1,112 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class apuParse implements  apuParseInterface {
+public class apuParse{
+	private String readin;
+	private String readout;
+	private BufferedReader br;
+	private FileReader fr;
+	private PrintWriter pw;
+	
+	private FileWriter fw;
+	private boolean ifRead;
+	public void
+	apuParse(String in, String out) {
+		readin = in;
+		readout = out;		
+	}
 
-	@Override
-	public void APUMAIN() {
+	public void read() {
+		BufferedReader br;
+		FileReader fr;
+		PrintWriter pw;
+		String line;
+		FileWriter fw;
+		try 												// code for reading in files
+		{
+            fr = new FileReader(readin);
+            fw = new FileWriter(readout);
+            br = new BufferedReader(fr);
+            pw = new PrintWriter(fw, true);
+            pw.print("Group Name(3) – Members(Micah Edington, Ryan Alcantra, Brandon Miller)\n");
+            pw.printf("%10s%15s\n", "Lexeme", "Token");
+            pw.println("    ------          -----");
+            ifRead = true;
+		}
+		catch (Exception e) 
+		{
+		
+			System.err.println(e);
+		}
+	}
+	
+	
+	private void APUMAIN() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public void APUINT() {
+	
+	private void APUINT() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public void APUFLOAT() {
+	private void APUFLOAT() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public void APUFUN() {
+	
+	private void APUFUN() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public void APUIF() {
+	
+	private void APUIF() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public void APUPRINT() {
+	private void APUPRINT() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public void APUELSE() {
+	
+	private void APUELSE() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public void APURETURN() {
+
+	private void APURETURN() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void APUWRITE() {
+	private void APUWRITE() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public void APUDOWHILE() {
+	private void APUDOWHILE() {
 		// TODO Auto-generated method stub
 		
+	}
+	private String nextline() {
+	 String line = "";
+		
+		return line;
+	}
+	public void close() throws IOException {
+		br.close();
+        pw.close();
+        System.out.println("Done!");
 	}
 
 }
